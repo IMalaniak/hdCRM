ced44916d5965fc44b4edbd586279305a442ce26
ArsenIrod <ArssenIrod@gmail.com>
2019-06-24 20:19:40 +0300

    #62 Home component mini landing

--- src/app/_modules/home/_components/landing/landing.component.ts
+++ src/app/_modules/home/_components/landing/landing.component.ts
@@ -6,33 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
-
   constructor() {
   }
   
-  ngOnInit() {  
-    const hamburger = document.querySelector(".hamburger");
-    const nav = document.querySelector("nav");
-
-    //Hamburger toggle
-    hamburger.addEventListener("click", () => {
-      hamburger.classList.toggle('active');
-      hamburger.classList.toggle('not-active');
-    });
- 
-    //Navbar shrink animation
-    window.onscroll = function() {
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        nav.classList.add('nav-shrink');
-      } else {
-        nav.classList.remove('nav-shrink');
-      }
-
-    //Navbar md remove shrink animation
-      if (window.matchMedia("(max-width: 991px)").matches) {
-        nav.classList.remove('nav-shrink');
-      }
-    };
-
+  ngOnInit() { 
   }
 }
