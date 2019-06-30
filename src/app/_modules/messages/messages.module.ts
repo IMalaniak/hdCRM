4f87ca6fa0fe7155030cd3e6301844c85283a965
JohnRostislavovich <admin@mywebmaster.pp.ua>
2019-06-30 18:09:37 +0200

    #63 common color scheme

--- src/app/_modules/messages/messages.module.ts
+++ src/app/_modules/messages/messages.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MessagesComponent } from './_components/messages.component';
 import { MessageService } from './_services/message.service';
+import { AppMaterialModule } from '@/_shared/modules';
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        AppMaterialModule
     ],
     declarations: [
         MessagesComponent
