580b9b4a3bcab038d033c6c77669a7f882dc65d8
Ivan Malaniak <admin@mywebmaster.pp.ua>
2020-04-16 10:56:32 +0200

    #131 - no need of this guard

--- webApp/src/app/core/core.module.ts
+++ webApp/src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
@@ -8,8 +8,6 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { RouterStateSerializer, StoreRouterConnectingModule, DefaultRouterStateSerializer } from '@ngrx/router-store';
-
-import { EnsureModuleLoadedOnceGuard } from './ensureModuleLoadedOnceGuard';
 
 import { httpInterceptorsProviders } from './_interceptors';
 
@@ -52,10 +50,4 @@ import { environment } from 'environments/environment';
     { provide: RouterStateSerializer, useClass: CustomSerializer }
   ]
 })
-export class CoreModule extends EnsureModuleLoadedOnceGuard {
-  // Ensure that CoreModule is only loaded into AppModule
-  // Looks for the module in the parent injector to see if it's already been loaded (only want it loaded once)
-  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
-    super(parentModule);
-  }
-}
+export class CoreModule {}
