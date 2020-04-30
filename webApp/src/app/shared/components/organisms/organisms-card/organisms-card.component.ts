903ea7602d19efd0988e5e0b9523ff04593b3732
ArsenIrod <32510362+ArsenIrod@users.noreply.github.com>
2020-04-30 19:31:59 +0300

    #141 fixed comment

--- webApp/src/app/shared/components/organisms/organisms-card/organisms-card.component.ts
+++ webApp/src/app/shared/components/organisms/organisms-card/organisms-card.component.ts
@@ -4,7 +4,7 @@ import { Component, Input } from '@angular/core';
   selector: 'organisms-card',
   template: `
     <div class="card" [ngClass]="{ shadow: !disableShadow }">
-      <h5 *ngIf="title" class="card-title">{{ title }}</h5>
+      <h5 *ngIf="cardTitle" class="card-title">{{ cardTitle }}</h5>
       <div class="card-body">
         <ng-content></ng-content>
       </div>
@@ -13,6 +13,6 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./organisms-card.component.scss']
 })
 export class OrganismsCardComponent {
-  @Input() title: string;
+  @Input() cardTitle: string;
   @Input() disableShadow = false;
 }
