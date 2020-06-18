4f7df54b72572d9254112d779e8f9c3a2e23942b
JohnRostislavovich <admin@mywebmaster.pp.ua>
2020-06-18 21:17:16 +0200

    #172 refactor jwt interceptor, repeat failed requests after token's been refreshed

--- webApp/src/app/core/_interceptors/index.ts
+++ webApp/src/app/core/_interceptors/index.ts
@@ -5,7 +5,7 @@ import { JwtInterceptor } from './jwt.interceptor';
 import { ApiPrefixInterceptor } from './api-prefix.interceptor';
 
 export const httpInterceptorsProviders = [
-  { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: ApiPrefixInterceptor, multi: true },
-  { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+  { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
 ];
