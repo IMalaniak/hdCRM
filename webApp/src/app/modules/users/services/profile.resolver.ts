b285cc964286a94226635ae9e54fc353148b0369
JohnRostislavovich <admin@mywebmaster.pp.ua>
2020-07-12 20:45:44 +0200

    fix(#206): "my profile" button was not working

--- webApp/src/app/modules/users/services/profile.resolver.ts
+++ webApp/src/app/modules/users/services/profile.resolver.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 import { User } from '../models';
 import { Store, select } from '@ngrx/store';
 import { AppState } from '@/core/reducers';
-import { take } from 'rxjs/operators';
+import { first } from 'rxjs/operators';
 import { currentUser } from '@/core/auth/store/auth.selectors';
 
 @Injectable()
@@ -13,6 +13,7 @@ export class ProfileResolver implements Resolve<Observable<User>> {
   constructor(private store: Store<AppState>) {}
 
   resolve(): Observable<User> {
-    return this.store.pipe(select(currentUser), take(2));
+    // TODO: @IMalaniak profile is not showing on page reload
+    return this.store.pipe(select(currentUser), first());
   }
 }
