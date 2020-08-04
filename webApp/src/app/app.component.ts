f8669bf746477304eabc62d48c22689c1a586f26
Arsenii <arssenirod@gmail.com>
2020-08-04 14:44:05 +0300

    feat(#231): added  custom scrollPositionRestoration

--- webApp/src/app/app.component.ts
+++ webApp/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { appRouterTransition } from '@/shared';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, Scroll } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+import { filter, delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +12,23 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
+
+  constructor(private router: Router, private viewportScroller: ViewportScroller) {
+    this.router.events
+      .pipe(
+        filter((e: any): e is Scroll => e instanceof Scroll),
+        delay(0)
+      )
+      .subscribe(e => {
+        if (e.position) {
+          this.viewportScroller.scrollToPosition(e.position);
+        } else if (e.anchor) {
+          this.viewportScroller.scrollToAnchor(e.anchor);
+        } else {
+          this.viewportScroller.scrollToPosition([0, 0]);
+          window.scrollTo(0, 0);
+        }
+      });
   }
 }
