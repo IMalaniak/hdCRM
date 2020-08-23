aa4d247aac999f626134b223c0808f934364a9ae
Arsenii <arssenirod@gmail.com>
2020-08-23 16:06:07 +0300

    refactor(#306): fixed comments

--- webApp/src/app/shared/components/atoms/atoms-integration-card/atoms-integration-card.component.ts
+++ webApp/src/app/shared/components/atoms/atoms-integration-card/atoms-integration-card.component.ts
@@ -3,8 +3,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'atoms-integration-card',
   template: `
-    <div class="integration-card" [style.background-color]="bgColor" [ngClass]="[classes]" (click)="onClick($event)">
-      <img src="{{ src }}" alt="{{ title }}" [style.max-width.%]="imgMWidth" [ngClass]="{ 'not-active': isEnabled }" />
+    <div class="integration-card" [ngClass]="[classes]" (click)="onClick($event)">
+      <img src="{{ src }}" alt="{{ title }}" />
     </div>
   `,
   styleUrls: ['./atoms-integration-card.component.scss']
@@ -14,11 +14,7 @@ export class AtomsIntegrationCardComponent {
 
   @Input() title = 'noimage';
 
-  @Input() imgMWidth: number;
-
-  @Input() bgColor: string;
-
-  @Input() cardClass: string;
+  @Input() cardClass = '';
 
   @Input() isEnabled: boolean;
 
@@ -29,6 +25,7 @@ export class AtomsIntegrationCardComponent {
   }
 
   get classes(): string {
-    return this.cardClass;
+    let active = this.isEnabled ? 'active' : '';
+    return (active += ` ${this.cardClass}`);
   }
 }
