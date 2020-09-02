04c7a3c4499542a0a5ab93e8a761411f4d06803d
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2020-09-02 13:22:51 +0200

    refactor(#192): move server files to separate folder + removed unused files

--- migrations/20190325125402-password-attributes-rename-column.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.renameColumn('PasswordAttributes', 'resetPasswordTokenExpire', 'tokenExpire');
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.renameColumn('PasswordAttributes', 'tokenExpire', 'resetPasswordTokenExpire');
-  }
-};
