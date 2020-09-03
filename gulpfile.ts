b1c7818b2a2153d5a5c4e7acc7af99ba7947377d
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2020-09-03 08:47:19 +0200

    refactor(#192): added gulpfiles

---
+++ gulpfile.ts
@@ -0,0 +1,86 @@
+import { parallel, series } from 'gulp';
+
+import { doRun, lint, task } from './gulpfile.functions';
+
+const modules = ['server', 'webApp', 'gulp'];
+const artifactModules = ['server', 'webApp'];
+
+modules.forEach((it) => {
+  const cwd = it === 'gulp' ? '.' : it;
+  task({
+    name: `${it}:lint`,
+    fct: lint(cwd, false),
+    desc: `Lints ${it}`,
+  });
+
+  task({
+    name: `${it}:format`,
+    alias: `${it[0]}f`, // e.g. 'wf' === 'webApp:format'
+    fct: lint(cwd, true),
+    desc: `Formats (force --fix for linting) ${it}`,
+  });
+
+  task({
+    name: `${it}:install`,
+    alias: `${it[0]}i`, // e.g. 'wi' === 'webApp:install'
+    fct: doRun('npm install', { cwd }),
+    desc: `Runs 'npm install' on ${it}`,
+  });
+
+  // task({
+  //   name: `${it}:audit`,
+  //   fct: doRun('npm audit fix', { cwd }),
+  //   desc: `Runs 'npm audit fix' on ${it}`,
+  // });
+});
+
+artifactModules.forEach((it) => {
+  task({
+    name: `${it}:pre-build`,
+    fct: series(`${it}:lint`),
+    desc: `Lints ${it}`,
+  });
+});
+
+// tslint:disable:no-var-requires
+require('./gulpfile.webApp');
+require('./gulpfile.server');
+// tslint:enable:no-var-requires
+
+/**
+ * "All" Tasks
+ */
+
+// task({
+//   name: 'testAll',
+//   fct: series('webApp:test', 'server:test'),
+// });
+
+task({
+  name: 'lintAll',
+  fct: parallel(modules.map((it) => `${it}:lint`)),
+});
+
+task({
+  name: 'formatAll',
+  alias: 'fa',
+  fct: parallel(modules.map((it) => `${it}:format`)),
+});
+
+task({
+  name: 'installAll',
+  alias: 'ia',
+  fct: parallel(modules.map((it) => `${it}:install`)),
+  desc: "runs 'npm install' on all submodules",
+});
+
+// task({
+//   name: 'auditAll',
+//   fct: parallel(modules.map((it) => `${it}:audit`)),
+//   desc: "runs 'npm audit fix' on all submodules",
+// });
+
+task({
+  name: 'buildAll',
+  fct: parallel(artifactModules.map((it) => `${it}:build`)),
+});
---
+++ gulpfile.webApp.ts
@@ -0,0 +1,63 @@
+import { series } from 'gulp';
+import { doRun, task } from './gulpfile.functions';
+
+const cwd = { cwd: 'webApp' };
+const devTimeoutSeconds = 24 * 60 * 60;
+const devOptions = {
+  commandTimeoutSeconds: devTimeoutSeconds,
+};
+
+task({
+  name: 'webApp:checkTypes',
+  fct: doRun('node_modules/.bin/tsc --noEmit', { ...cwd, ...devOptions }),
+  desc: 'Check the webApp code typings (run tsc)',
+});
+
+task({
+  name: 'webApp:compile',
+  fct: series(
+    'webApp:checkTypes',
+    doRun('NODE_ENV=production node_modules/.bin/webpack', {
+      ...cwd,
+      ...devOptions,
+    })
+  ),
+  desc: 'Build the webApp application (run webpack)',
+});
+
+task({
+  name: 'webApp:compile-dev',
+  fct: series(
+    'webApp:checkTypes',
+    doRun('NODE_ENV=development node_modules/.bin/webpack', {
+      ...cwd,
+      ...devOptions,
+    })
+  ),
+  desc: 'Build the webApp application in development mode (run webpack)',
+});
+
+task({
+  name: 'webApp:build',
+  fct: series('webApp:install', 'webApp:compile'),
+  desc: 'Install all dependencies and compile the application',
+});
+
+task({
+  name: 'webApp:build-dev',
+  fct: series('webApp:install', 'webApp:compile-dev'),
+  desc: 'Install all dependencies and compile the application in development mode',
+});
+
+task({
+  name: 'webApp:dev',
+  alias: 'cd',
+  fct: series(
+    'webApp:checkTypes',
+    doRun(
+      'NODE_ENV=development node_modules/.bin/webpack-dev-server --host 0.0.0.0 --inline --watch --hot --port 3333',
+      { ...cwd, ...devOptions }
+    )
+  ),
+  desc: 'Start the webApp dev server on port 3333',
+});
