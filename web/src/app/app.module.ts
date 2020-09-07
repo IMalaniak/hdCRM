2566c51e69d371f07432557434a7742d5f6d7218
Arsenii Irod <ArseniiIrod@gmail.com>
2020-09-07 21:31:27 +0300

    perf(#328): fixed PR comment

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -6,14 +6,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 
-import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
-
 import { AppComponent } from './app.component';
 import { SocketService } from './shared/services';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, CoreModule, AppRoutingModule, SweetAlert2Module.forRoot()],
+  imports: [BrowserModule, BrowserAnimationsModule, CoreModule, AppRoutingModule],
   providers: [SocketService],
   bootstrap: [AppComponent]
 })
