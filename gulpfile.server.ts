903530368af17a302ff6c605f3690cc7e896ca64
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2020-10-02 17:02:55 +0200

    refactor(#439): fixed tasks order

--- gulpfile.server.ts
+++ gulpfile.server.ts
@@ -3,43 +3,16 @@ import { doRun, task } from './gulpfile.functions';
 
 const devTimeoutSeconds = 24 * 60 * 60;
 
-task({
-  name: 'server:compile',
-  alias: 'sc',
-  fct: doRun('node_modules/.bin/tsc --project tsconfig.prod.json', { cwd: `${__dirname}/server` }),
-  desc: 'Compiles *.ts => *.js'
-});
-
-task({
-  name: 'server:build',
-  alias: 'sb',
-  fct: series('server:install', 'server:compile', 'server:prepareProdDB'),
-  desc: 'Builds all server artifacts (runs tsc, npm install), and prepares bd'
-});
-
-task({
-  name: 'server:buildProd',
-  alias: 'sb',
-  fct: series('server:unit-test', 'server:compile', 'server:prepareProdDB'),
-  desc: 'Runs all server unit tests and then builds all server artifacts, and prepares bd'
-});
-
 const devOptions = {
   cwd: `${__dirname}/server`,
   commandTimeoutSeconds: devTimeoutSeconds
 };
 
 task({
-  name: 'server:start',
-  fct: series('server:compile', doRun('node dist/index.js', devOptions)),
-  desc: 'Starts the server'
-});
-
-task({
-  name: 'server:dev',
-  alias: 'sd',
-  fct: doRun('node_modules/.bin/nodemon', devOptions),
-  desc: 'Start the dev server'
+  name: 'server:compile',
+  alias: 'sc',
+  fct: doRun('node_modules/.bin/tsc --project tsconfig.prod.json', { cwd: `${__dirname}/server` }),
+  desc: 'Compiles *.ts => *.js'
 });
 
 task({
@@ -59,4 +32,31 @@ task({
     doRun('node_modules/.bin/sequelize db:seed:all', { cwd: `${__dirname}/server` })
   ),
   desc: 'Prepare database from production or test'
+});
+
+task({
+  name: 'server:build',
+  alias: 'sb',
+  fct: series('server:install', 'server:compile', 'server:prepareProdDB'),
+  desc: 'Builds all server artifacts (runs tsc, npm install), and prepares bd'
+});
+
+task({
+  name: 'server:buildProd',
+  alias: 'sb',
+  fct: series('server:unit-test', 'server:compile', 'server:prepareProdDB'),
+  desc: 'Runs all server unit tests and then builds all server artifacts, and prepares bd'
+});
+
+task({
+  name: 'server:start',
+  fct: series('server:compile', doRun('node dist/index.js', devOptions)),
+  desc: 'Starts the server'
+});
+
+task({
+  name: 'server:dev',
+  alias: 'sd',
+  fct: doRun('node_modules/.bin/nodemon', devOptions),
+  desc: 'Start the dev server'
 });
