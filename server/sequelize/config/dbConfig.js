7f77f7629b617199a5f081c4e34d17e035589425
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2020-10-03 11:24:46 +0200

    fix(#439): remove dialect from config

--- server/sequelize/config/dbConfig.js
+++ server/sequelize/config/dbConfig.js
@@ -1,13 +1,11 @@
 module.exports = {
   development: {
     use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
     migrationStorage: 'sequelize',
     seederStorage: 'sequelize'
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
     dialectOptions: {
       ssl: true,
       rejectUnauthorized: false
@@ -17,7 +15,6 @@ module.exports = {
   },
   test: {
     use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
     migrationStorage: 'sequelize',
     seederStorage: 'sequelize'
   }
