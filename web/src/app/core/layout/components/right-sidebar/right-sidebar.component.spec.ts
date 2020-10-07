22d897b7b62c9d03313e05cfa3898e8383a55d9c
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2020-10-07 16:08:44 +0200

    fix(#273): remove myself from usersonline when logged in from different device

--- web/src/app/core/layout/components/right-sidebar/right-sidebar.component.spec.ts
+++ web/src/app/core/layout/components/right-sidebar/right-sidebar.component.spec.ts
@@ -6,12 +6,15 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { RightSidebarComponent } from './right-sidebar.component';
 
 import { provideMockStore } from '@ngrx/store/testing';
-import { initialUsersState } from '@/modules/users/store/user.reducer';
+import { initialUsersState, UsersState } from '@/modules/users/store/user.reducer';
+import { authStateMock } from '@/shared/testing/mocks';
+import { AuthState } from '@/core/auth/store/auth.reducer';
 
 describe('RightSidebarComponent', () => {
   let component: RightSidebarComponent;
   let fixture: ComponentFixture<RightSidebarComponent>;
-  const initialState = {
+  const initialState: { auth: AuthState; users: UsersState } = {
+    auth: authStateMock,
     users: initialUsersState
   };
 
