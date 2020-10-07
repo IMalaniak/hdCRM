7a48153274c8264c04a7a31cf6fae0964688e51a
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2020-10-07 14:43:09 +0200

    fix(#289): fixed failed login attempt after error and refresh session

--- web/src/app/core/_guards/auth.guard.ts
+++ web/src/app/core/_guards/auth.guard.ts
@@ -3,23 +3,26 @@ import { CanActivate } from '@angular/router';
 import { Store, select } from '@ngrx/store';
 import { AppState } from '../reducers';
 import { Observable } from 'rxjs';
-import { tap, skipWhile } from 'rxjs/operators';
-import { isLoggedIn, isLoading } from '../auth/store/auth.selectors';
+import { tap, withLatestFrom, map, take } from 'rxjs/operators';
+import { isTokenValid } from '../auth/store/auth.selectors';
 
-import { redirectToLogin, checkIsTokenValid } from '../auth/store/auth.actions';
+import { checkIsTokenValid, redirectToLogin } from '../auth/store/auth.actions';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
   constructor(private store$: Store<AppState>) {}
 
   canActivate(): Observable<boolean> {
-    this.store$.dispatch(checkIsTokenValid());
     return this.store$.pipe(
-      skipWhile(isLoading),
-      select(isLoggedIn),
+      // tap(() => ),
+      withLatestFrom(this.store$.pipe(select(isTokenValid))),
+      take(1),
+      map(([_, valid]) => valid),
       tap((loggedIn) => {
         if (!loggedIn) {
           this.store$.dispatch(redirectToLogin());
+        } else {
+          this.store$.dispatch(checkIsTokenValid());
         }
       })
     );
