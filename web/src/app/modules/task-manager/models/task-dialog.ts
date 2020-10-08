e395c57dbe5c42d0a208337ed93bd3d1f9827f4d
ArseniiIrod <arssenirod@gmail.com>
2020-10-08 22:11:15 +0300

    refactor(#455): removed dialog interface

--- web/src/app/modules/task-manager/models/task-dialog.ts
+++
@@ -1,8 +0,0 @@
-import { Task } from './task';
-import { TaskPriority } from './task-priority';
-
-export interface TaskDialogData {
-  title: string;
-  task?: Task;
-  priorities: TaskPriority[];
-}
