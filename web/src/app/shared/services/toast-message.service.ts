af60249c80eaae46177c296617d83604373db9a9
ArseniiIrod <arssenirod@gmail.com>
2020-10-08 21:43:54 +0300

    refactor(#455): removed unnecessary methods

--- web/src/app/shared/services/toast-message.service.ts
+++ web/src/app/shared/services/toast-message.service.ts
@@ -1,53 +1,20 @@
 import { Injectable } from '@angular/core';
-import Swal, { SweetAlertIcon } from 'sweetalert2';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+
 import { MoleculesServerMessageComponent } from '../components/molecules';
 import { ApiResponse } from '../models';
-import { DIALOG, ALERT } from '../constants';
-
 @Injectable({
   providedIn: 'root'
 })
 export class ToastMessageService {
-  constructor(public snackBar: MatSnackBar) {}
-
-  confirm(title: string, text: string) {
-    return Swal.fire({
-      title,
-      text,
-      icon: ALERT.QUESTION,
-      showCancelButton: true,
-      confirmButtonText: DIALOG.YES,
-      cancelButtonText: DIALOG.CANCEL
-    });
-  }
-
-  toast(title: string, icon: SweetAlertIcon = ALERT.SUCCESS, timer: number = 2500) {
-    return Swal.fire({
-      title,
-      toast: true,
-      icon,
-      timer,
-      showCancelButton: false,
-      showCloseButton: false,
-      showConfirmButton: false,
-      position: 'bottom-end'
-    });
-  }
-
-  popup(title: string, icon: SweetAlertIcon, timer: number = 2500) {
-    return Swal.fire({
-      title,
-      icon,
-      timer
-    });
-  }
+  constructor(private readonly _snackBar: MatSnackBar) {}
 
   snack(serverResponse: ApiResponse, duration = 5000): void {
-    this.snackBar.openFromComponent(MoleculesServerMessageComponent, {
+    const config: MatSnackBarConfig = {
       data: serverResponse,
       duration,
       panelClass: 'p-0'
-    });
+    };
+    this._snackBar.openFromComponent(MoleculesServerMessageComponent, config);
   }
 }
