68e3d1940211ec452c04f06e98edf4fbed8aa627
ArseniiIrod <arssenirod@gmail.com>
2020-10-08 22:04:30 +0300

    refactor($455):  removed alert constant, added  data into constants

--- web/src/app/shared/constants/dialog.constants.ts
+++ web/src/app/shared/constants/dialog.constants.ts
@@ -1,7 +1,11 @@
-export enum DIALOG {
-  CONFIRM = 'Are You sure?',
-  OK = 'OK',
-  CANCEL = 'Cancel',
-  YES = 'Yes',
-  NO = 'No'
+export class DIALOG {
+  static CONFIRM = 'Are You sure?';
+  static OK = 'OK';
+  static CANCEL = 'Cancel';
+  static YES = 'Yes';
+  static NO = 'No';
+  static SAVE = 'Save';
+  static CREATE = 'Create';
+  static MAX_WIDTH = '90vw';
+  static MAX_HEIGHT = '90vh';
 }
