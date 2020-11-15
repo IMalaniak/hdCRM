8e7e84d5e03b8251c2a043829d79162e5aa49c32
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2020-11-15 20:40:02 +0100

    refactor(#561): use matNoDataRow instead of custom checker

--- web/src/app/shared/services/common.datasource.ts
+++ web/src/app/shared/services/common.datasource.ts
@@ -1,7 +1,6 @@
 import { DataSource } from '@angular/cdk/collections';
 
 import { Observable, BehaviorSubject } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 export class CommonDataSource<T> implements DataSource<T> {
   protected listSubject = new BehaviorSubject<T[]>([]);
@@ -12,9 +11,5 @@ export class CommonDataSource<T> implements DataSource<T> {
 
   disconnect(): void {
     this.listSubject.complete();
-  }
-
-  isEmpty(): Observable<boolean> {
-    return this.connect().pipe(map((data) => data.length === 0));
   }
 }
