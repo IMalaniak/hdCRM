0ceeb50db4709a64099ff11b78cbfcc9d8fcd3b0
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2020-11-18 10:16:57 +0100

    refactor(#561): simplified no-content icon atom

--- web/src/app/shared/components/atoms/atoms-no-content-info/atoms-no-content-info.component.ts
+++ web/src/app/shared/components/atoms/atoms-no-content-info/atoms-no-content-info.component.ts
@@ -1,23 +1,19 @@
-import { Component, ChangeDetectionStrategy, Input, HostBinding } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 import { CONSTANTS } from '@/shared/constants';
 
 @Component({
-  selector: 'atoms-no-content-info, [atoms-no-content-info]',
-  template: `
-    <td colspan="100" *ngIf="wrap; else unwrapped">
-      <ng-content *ngTemplateOutlet="unwrapped"> </ng-content>
-    </td>
-    <ng-template #unwrapped>
-      <p class="lead p-4 text-center">{{ noContentInfo }}</p>
-    </ng-template>
-  `,
+  selector: 'atoms-no-content-info',
+  template: ` <p class="lead p-4 mb-0 text-center">{{ noContentInfo }}</p> `,
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AtomsNoContentInfoComponent {
-  @Input() wrap: boolean;
-  @HostBinding('class.d-block') get displayBlock(): boolean {
-    return !this.wrap;
-  }
   noContentInfo = CONSTANTS.NO_CONTENT_INFO;
 }
