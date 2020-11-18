e5f4ac05f202ad2aa93af5e2b5236ff12dab1e87
ArseniiIrod <arssenirod@gmail.com>
2020-11-19 00:22:32 +0200

    fix(#455): added missed export

--- web/src/app/shared/services/index.ts
+++ web/src/app/shared/services/index.ts
@@ -3,3 +3,4 @@ export * from './common.datasource';
 export * from './local-storage.service';
 export * from './socket.service';
 export * from './toast-message.service';
+export * from './dynamic-form.resolver';
