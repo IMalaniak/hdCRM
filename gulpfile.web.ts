2795e9c57d45446c024fd21ce6079456109760b8
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2020-11-21 18:56:19 +0100

    feat(#615): added script for icons copy from bootstrap

--- gulpfile.web.ts
+++ gulpfile.web.ts
@@ -1,10 +1,14 @@
 import { series } from 'gulp';
-import { doRun, task } from './gulpfile.functions';
+import { copy, doRun, task } from './gulpfile.functions';
 
 const cwd = { cwd: `${__dirname}/web` };
 const devTimeoutSeconds = 24 * 60 * 60;
 const devOptions = {
   commandTimeoutSeconds: devTimeoutSeconds
+};
+
+const copyBSIcons = () => {
+  return copy('node_modules/bootstrap-icons/icons/*.svg', 'src/assets/images/bs-icons', cwd);
 };
 
 task({
@@ -44,8 +48,15 @@ task({
 });
 
 task({
+  name: 'web:dev-assets',
+  alias: 'wda',
+  fct: copyBSIcons,
+  desc: 'Start the web dev server on port 3333'
+});
+
+task({
   name: 'web:dev',
   alias: 'wd',
-  fct: doRun('node_modules/.bin/ng serve -o', { ...cwd, ...devOptions }),
+  fct: series('web:dev-assets', doRun('node_modules/.bin/ng serve -o', { ...cwd, ...devOptions })),
   desc: 'Start the web dev server on port 3333'
 });
