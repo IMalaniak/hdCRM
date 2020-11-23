95cc240a9331d357c79ae709926b0c8eb682ab25
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2020-11-23 17:16:24 +0100

    refactor(#630): changed icons in footer

--- web/src/app/core/layout/components/footer/footer.component.ts
+++ web/src/app/core/layout/components/footer/footer.component.ts
@@ -1,9 +1,24 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 
+import { IconsService } from '@/core/services';
+import { ICONS } from '@/shared/constants';
+
 @Component({
   selector: 'footer-component',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FooterComponent {}
+export class FooterComponent {
+  socialIcons = {
+    email: ICONS.SOCIAL_Email,
+    fb: ICONS.SOCIAL_Facebook,
+    ig: ICONS.SOCIAL_Instagram,
+    linkedIn: ICONS.SOCIAL_LinkedIn,
+    tw: ICONS.SOCIAL_Twitter,
+    telegram: ICONS.SOCIAL_Telegram
+  };
+  constructor(private iconsService: IconsService) {
+    this.iconsService.registerIcons([...Object.values(this.socialIcons)]);
+  }
+}
