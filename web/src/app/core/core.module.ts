0984254b2ebb5ad5b7a104cb060d854091d35b3f
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2020-11-25 17:27:15 +0100

    fix(#278): remove unused router serializer

--- web/src/app/core/core.module.ts
+++ web/src/app/core/core.module.ts
@@ -7,10 +7,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { StoreRouterConnectingModule, RouterState } from '@ngrx/router-store';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
-import { CustomSerializer } from '@/shared/utils';
-import { LocalStorageService } from '@/shared/services';
 import { environment } from 'environments/environment';
 import { httpInterceptorsProviders } from './_interceptors';
 import { AuthModule } from './auth/auth.module';
@@ -19,8 +17,6 @@ import { reducers, metaReducers } from './reducers';
 import { PreferencesEffects } from './reducers/preferences.effects';
 import { IntegrationsEffects } from './reducers/integration.effects';
 import { DynamicFormEffects } from './reducers/dynamic-form/dynamic-form.effects';
-import { DynamicFormService } from './services/dynamic-form.service';
-import { MediaQueryService } from './services';
 
 @NgModule({
   imports: [
@@ -40,13 +36,9 @@ import { MediaQueryService } from './services';
     }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     EffectsModule.forRoot([PreferencesEffects, IntegrationsEffects, DynamicFormEffects]),
-    StoreRouterConnectingModule.forRoot({
-      serializer: CustomSerializer,
-      stateKey: 'router',
-      routerState: RouterState.Full
-    })
+    StoreRouterConnectingModule.forRoot()
   ],
   exports: [RouterModule, HttpClientModule, FormsModule, ReactiveFormsModule, LayoutModule],
-  providers: [httpInterceptorsProviders, MediaQueryService, DynamicFormService, LocalStorageService]
+  providers: [httpInterceptorsProviders]
 })
 export class CoreModule {}
