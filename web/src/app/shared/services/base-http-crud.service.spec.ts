3fc71619e9238e085ee728cda9b5ba330dbb7e92
ArseniiIrod <arssenirod@gmail.com>
2020-11-28 23:34:14 +0200

    test($447): added test for getOne method

--- web/src/app/shared/services/base-http-crud.service.spec.ts
+++ web/src/app/shared/services/base-http-crud.service.spec.ts
@@ -1,20 +1,39 @@
+import { User } from '@/modules/users';
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { BaseCrudService } from './base-http-crud.service';
+import { currentUserMock } from '../testing/mocks/store.mock';
+import { ItemApiResponse } from '../models/apiResponse';
 
 describe('BaseCrudService', () => {
-  let service: BaseCrudService;
+  class TestCrudService extends BaseCrudService {
+    protected readonly url = '/test-api';
+  }
+
+  let httpClientSpy: { get: jasmine.Spy };
+  let service: TestCrudService;
+  const user: User = currentUserMock;
 
   beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new TestCrudService(httpClientSpy as any);
+
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      providers: [BaseCrudService]
+      providers: [TestCrudService]
     });
-    service = TestBed.inject(BaseCrudService);
+    service = TestBed.inject(TestCrudService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return expected item', () => {
+    httpClientSpy.get.and.returnValue(of({ success: true, data: user }));
+
+    service.getOne<User>(1).subscribe((response: ItemApiResponse<User>) => expect(response.data).toBe(user));
   });
 });
