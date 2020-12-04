3e3d1b6d3de236d8f13c7601b6c1b985061d6975
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2020-12-04 10:52:13 +0100

    feat(#548): implemented logger class

---
+++ server/src/utils/Logger.ts
@@ -0,0 +1,52 @@
+import pino from 'pino';
+import { Service } from 'typedi';
+
+@Service({ global: true })
+export class Logger {
+  private logger = pino({
+    prettyPrint:
+      process.env.NODE_ENV === 'development'
+        ? {
+            levelFirst: true,
+            colorize: true
+          }
+        : false
+  });
+
+  public trace(message: string): void {
+    this.logger[LogLevel.TRACE](message);
+  }
+
+  public debug(message: string): void {
+    this.logger[LogLevel.DEBUG](message);
+  }
+
+  public info(message: string): void {
+    this.logger[LogLevel.INFO](message);
+  }
+
+  public warn(message: string): void {
+    this.logger[LogLevel.WARN](message);
+  }
+
+  public error(message: string): void {
+    this.logger[LogLevel.ERROR](message);
+  }
+
+  public fatal(message: string): void {
+    this.logger[LogLevel.FATAL](message);
+  }
+
+  public get instance() {
+    return this.logger;
+  }
+}
+
+enum LogLevel {
+  TRACE = 'trace',
+  DEBUG = 'debug',
+  INFO = 'info',
+  WARN = 'warn',
+  ERROR = 'error',
+  FATAL = 'fatal'
+}
