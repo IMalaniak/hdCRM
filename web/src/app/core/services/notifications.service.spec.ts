8a1d80575dbdcfd3c0152332e2a245f7cbb550ae
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2020-12-13 13:50:04 +0100

    test(#674): added unit tests

--- web/src/app/core/services/notifications.service.spec.ts
+++ web/src/app/core/services/notifications.service.spec.ts
@@ -1,16 +1,110 @@
-import { TestBed } from '@angular/core/testing';
+import { NOTIFICATION_TYPES } from '@/shared/constants';
+import { DateUtilityService } from '@/shared/services';
+import { Notification } from '@/shared/models';
 
 import { NotificationsService } from './notifications.service';
 
 describe('NotificationsService', () => {
   let service: NotificationsService;
+  let localStorageServiceSpy: {
+    getObject: jasmine.Spy;
+    setObjectKeyValue: jasmine.Spy;
+  };
+
+  const notificationLocalStorage = {
+    list: [
+      {
+        id: '1',
+        description: 'Test description',
+        type: NOTIFICATION_TYPES.WARN,
+        read: false,
+        date: new Date('2020-12-14T11:04:06.317Z')
+      },
+      {
+        id: '2',
+        description: 'Test 2 description',
+        type: NOTIFICATION_TYPES.INFO,
+        read: true,
+        date: new Date('2020-12-14T11:04:06.317Z')
+      }
+    ],
+    ignoreList: {
+      passwordExpire: new Date('2020-12-10T11:04:06.317Z')
+    }
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(NotificationsService);
+    localStorageServiceSpy = jasmine.createSpyObj('LocalStorageService', ['getObject', 'setObjectKeyValue']);
+    localStorageServiceSpy.getObject.and.returnValue(notificationLocalStorage);
+    service = new NotificationsService(localStorageServiceSpy as any, new DateUtilityService());
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return notification list', () => {
+    expect(service.getList()).toEqual(notificationLocalStorage.list);
+  });
+
+  it('should return ignore list', () => {
+    expect(service.getIgnoreList()).toEqual(notificationLocalStorage.ignoreList);
+  });
+
+  it('should create a notification', () => {
+    const expected: Partial<Notification> = {
+      description: 'New notification',
+      type: NOTIFICATION_TYPES.INFO,
+      read: false
+    };
+
+    expect(service.create('New notification', NOTIFICATION_TYPES.INFO)).toEqual(jasmine.objectContaining(expected));
+  });
+
+  it('should mark notification as read', () => {
+    service.markAsRead('1');
+    expect(localStorageServiceSpy.setObjectKeyValue).toHaveBeenCalledOnceWith('notifications', 'list', [
+      {
+        id: '1',
+        description: 'Test description',
+        type: NOTIFICATION_TYPES.WARN,
+        read: true,
+        date: new Date('2020-12-14T11:04:06.317Z')
+      },
+      {
+        id: '2',
+        description: 'Test 2 description',
+        type: NOTIFICATION_TYPES.INFO,
+        read: true,
+        date: new Date('2020-12-14T11:04:06.317Z')
+      }
+    ]);
+  });
+
+  it('should remove notification', () => {
+    service.remove('1');
+    expect(localStorageServiceSpy.setObjectKeyValue).toHaveBeenCalledOnceWith('notifications', 'list', [
+      {
+        id: '2',
+        description: 'Test 2 description',
+        type: NOTIFICATION_TYPES.INFO,
+        read: true,
+        date: new Date('2020-12-14T11:04:06.317Z')
+      }
+    ]);
+  });
+
+  it('should set a new ignore notification', () => {
+    const newIgnore = { ['newIgnore']: new Date('2020-12-14T11:04:06.317Z') };
+    service.ignore(newIgnore);
+    expect(localStorageServiceSpy.setObjectKeyValue).toHaveBeenCalledOnceWith('notifications', 'ignoreList', {
+      ...notificationLocalStorage.ignoreList,
+      ...newIgnore
+    });
+  });
+
+  it('should check ignore', () => {
+    expect(service.checkIgnore('passwordExpire')).toBeFalse();
+    expect(service.checkIgnore('not-exist')).toBeFalse();
   });
 });
--- web/src/app/shared/services/date-utility.service.spec.ts
+++ web/src/app/shared/services/date-utility.service.spec.ts
@@ -13,4 +13,16 @@ describe('DateUtilityService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should show difference from today in days', () => {
+    const target = new Date();
+    target.setDate(target.getDate() + 1);
+    expect(service.diffDaysFromToday(target)).toBe(1);
+  });
+
+  it('should add one day from today', () => {
+    const target = new Date();
+    target.setDate(target.getDate() + 1);
+    expect(service.addFutureDays(1)).toEqual(target);
+  });
 });
