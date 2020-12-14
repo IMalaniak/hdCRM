3341945d1522458f1111f065fa3babafb97fb873
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2020-12-14 15:58:47 +0100

    refactor(#705): use table component in plan list

--- web/src/app/modules/planner/components/list/plan-list.component.ts
+++ web/src/app/modules/planner/components/list/plan-list.component.ts
@@ -1,6 +1,5 @@
-import { Component, AfterViewInit, ViewChild, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
-import { Observable, Subject, merge } from 'rxjs';
-import { MatSort } from '@angular/material/sort';
+import { Component, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 import { SelectionModel } from '@angular/cdk/collections';
 import { Router } from '@angular/router';
 
@@ -9,11 +8,8 @@ import { Store, select } from '@ngrx/store';
 import { AppState } from '@/core/reducers';
 import { PlansDataSource } from '../../services/plan.datasource';
 import { selectPlanPageLoading, selectPlansTotalCount } from '../../store/plan.selectors';
-import { MatPaginator } from '@angular/material/paginator';
-import { tap, takeUntil } from 'rxjs/operators';
-import { DialogDataModel, PageQuery } from '@/shared/models';
+import { DialogDataModel } from '@/shared/models';
 import {
-  IItemsPerPage,
   pageSizeOptions,
   COLUMN_LABELS,
   ACTION_LABELS,
@@ -25,31 +21,26 @@ import {
 } from '@/shared/constants';
 import { isPrivileged } from '@/core/auth/store/auth.selectors';
 import { deletePlanRequested, changeIsEditingState } from '../../store/plan.actions';
-import { getItemsPerPageState } from '@/core/reducers/preferences.selectors';
-import { SORT_DIRECTION, ADD_PRIVILEGES, EDIT_PRIVILEGES, DELETE_PRIVILEGES, COLUMN_NAMES } from '@/shared/constants';
+import { ADD_PRIVILEGES, EDIT_PRIVILEGES, DELETE_PRIVILEGES, COLUMN_NAMES } from '@/shared/constants';
 import { DialogConfirmModel } from '@/shared/models/dialog/dialog-confirm.model';
 import { DialogConfirmComponent } from '@/shared/components/dialogs/dialog-confirm/dialog-confirm.component';
 import { DialogService } from '@/shared/services';
+import { DataColumn } from '@/shared/models/table';
 
 @Component({
   templateUrl: './plan-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PlanListComponent implements AfterViewInit, OnDestroy {
+export class PlanListComponent implements OnDestroy {
   dataSource: PlansDataSource = new PlansDataSource(this.store$);
   loading$: Observable<boolean> = this.store$.pipe(select(selectPlanPageLoading));
   resultsLength$: Observable<number> = this.store$.pipe(select(selectPlansTotalCount));
   canAddPlan$: Observable<boolean> = this.store$.pipe(select(isPrivileged(ADD_PRIVILEGES.PLAN)));
   canEditPlan$: Observable<boolean> = this.store$.pipe(select(isPrivileged(EDIT_PRIVILEGES.PLAN)));
   canDeletePlan$: Observable<boolean> = this.store$.pipe(select(isPrivileged(DELETE_PRIVILEGES.PLAN)));
-  itemsPerPageState$: Observable<IItemsPerPage> = this.store$.pipe(select(getItemsPerPageState));
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
 
   selection = new SelectionModel<Plan>(true, []);
   pageSizeOptions: number[] = pageSizeOptions;
-  columns = COLUMN_NAMES;
   columnLabels = COLUMN_LABELS;
   actionLabels = ACTION_LABELS;
   themePalette = THEME_PALETTE;
@@ -63,42 +54,18 @@ export class PlanListComponent implements AfterViewInit, OnDestroy {
     delete: BS_ICONS.Trash
   };
 
-  displayedColumns: COLUMN_NAMES[] = [
-    COLUMN_NAMES.TITLE,
-    COLUMN_NAMES.CREATOR,
-    COLUMN_NAMES.STAGE,
-    COLUMN_NAMES.PARTICIPANTS,
-    COLUMN_NAMES.CREATED_AT,
-    COLUMN_NAMES.UPDATED_AT,
-    COLUMN_NAMES.DEADLINE,
-    COLUMN_NAMES.ACTIONS
+  displayedColumns: DataColumn[] = [
+    new DataColumn(COLUMN_NAMES.TITLE),
+    new DataColumn(COLUMN_NAMES.STAGE),
+    new DataColumn(COLUMN_NAMES.CREATOR),
+    new DataColumn(COLUMN_NAMES.PARTICIPANTS),
+    new DataColumn(COLUMN_NAMES.CREATED_AT),
+    new DataColumn(COLUMN_NAMES.UPDATED_AT),
+    DataColumn.createActionsColumn()
   ];
   private unsubscribe: Subject<void> = new Subject();
 
   constructor(private store$: Store<AppState>, private router: Router, private dialogService: DialogService) {}
-
-  ngAfterViewInit(): void {
-    const sort$ = this.sort.sortChange.pipe(tap(() => (this.paginator.pageIndex = 0)));
-    merge(sort$, this.paginator.page)
-      .pipe(
-        takeUntil(this.unsubscribe),
-        tap(() => this.loadPlansPage())
-      )
-      .subscribe();
-
-    this.loadPlansPage();
-  }
-
-  loadPlansPage(): void {
-    const newPage: PageQuery = {
-      pageIndex: this.paginator.pageIndex,
-      pageSize: this.paginator.pageSize,
-      sortIndex: this.sort.active || COLUMN_NAMES.ID,
-      sortDirection: this.sort.direction || SORT_DIRECTION.ASC
-    };
-
-    this.dataSource.loadData(newPage);
-  }
 
   onPlanSelect(id: number, edit: boolean = false): void {
     this.router.navigateByUrl(`${RoutingConstants.ROUTE_PLANNER_DETAILS}/${id}`);
