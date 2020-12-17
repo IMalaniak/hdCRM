c5b65c9a5bc110ff0dafcf96958bb0d6c1842bf9
ArseniiIrod <arssenirod@gmail.com>
2020-12-18 00:28:41 +0200

    refactor(#705): changed number to id

--- web/src/app/shared/utils/url.generator.ts
+++ web/src/app/shared/utils/url.generator.ts
@@ -2,11 +2,11 @@ import { RoutingConstants } from '../constants/routing.constants';
 import { createNavigation, Navigation } from '../models/table/navigation';
 
 export class UrlGenerator {
-  static getUserUrl(number: number): Navigation {
-    return createNavigation([`/${RoutingConstants.ROUTE_USERS_DETAILS}/${number}`]);
+  static getUserUrl(id: number): Navigation {
+    return createNavigation([`/${RoutingConstants.ROUTE_USERS_DETAILS}/${id}`]);
   }
 
-  static getDepartmentUrl(number: number): Navigation {
-    return createNavigation([`/${RoutingConstants.ROUTE_DEPARTMENTS_DETAILS}/${number}`]);
+  static getDepartmentUrl(id: number): Navigation {
+    return createNavigation([`/${RoutingConstants.ROUTE_DEPARTMENTS_DETAILS}/${id}`]);
   }
 }
