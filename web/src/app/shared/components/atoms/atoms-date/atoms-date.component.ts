d37933448cb5c0bf3c92b36b386a31c83db6cef8
ArseniiIrod <arssenirod@gmail.com>
2021-01-30 11:43:16 +0200

    fix(#804): removed no existed file reference

--- web/src/app/shared/components/atoms/atoms-date/atoms-date.component.ts
+++ web/src/app/shared/components/atoms/atoms-date/atoms-date.component.ts
@@ -1,13 +1,13 @@
-import { InputType } from '@/shared/constants';
 import { Component, Input, Optional, Self } from '@angular/core';
-import { NgControl } from '@angular/forms';
 import { MatFormFieldControl } from '@angular/material/form-field';
-import { BaseControlValueAccessorComponentModel } from '../../base';
+import { NgControl } from '@angular/forms';
+
+import { BaseControlValueAccessorComponentModel } from '../../base/componentModels';
+import { InputType } from '@/shared/constants';
 
 @Component({
   selector: 'atoms-date',
   templateUrl: './atoms-date.component.html',
-  styleUrls: ['./atoms-date.component.scss'],
   providers: [{ provide: MatFormFieldControl, useExisting: AtomsDateComponent }]
 })
 export class AtomsDateComponent extends BaseControlValueAccessorComponentModel<Date | null | undefined> {
