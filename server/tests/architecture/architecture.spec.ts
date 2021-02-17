96df5fbced191bd6e39df14409de750d341dd411
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2021-02-17 15:54:42 +0100

    test(#859): added specs to arch test

--- server/tests/architecture/architecture.spec.ts
+++ server/tests/architecture/architecture.spec.ts
@@ -16,7 +16,7 @@ describe('Architecture tests', () => {
       .inFolder('controllers')
       .matchingPattern(doesNotMatchRegex(['utils', 'index.ts']))
       .should()
-      .matchPattern('.*Controller.ts')
+      .matchPattern('.*Controller.ts|.*Controller.spec.ts')
       .check();
 
     expect(violations).deep.equal([]);
@@ -27,7 +27,7 @@ describe('Architecture tests', () => {
       .inFolder('services')
       .matchingPattern(doesNotMatchRegex(['utils', 'index.ts']))
       .should()
-      .matchPattern('.*Service.ts')
+      .matchPattern('.*Service.ts|.*Service.spec.ts')
       .check();
 
     expect(violations).deep.equal([]);
@@ -38,7 +38,7 @@ describe('Architecture tests', () => {
       .inFolder('routes')
       .matchingPattern(doesNotMatchRegex(['index.ts']))
       .should()
-      .matchPattern('.*Routes.ts')
+      .matchPattern('.*Routes.ts|.*Routes.spec.ts')
       .check();
 
     expect(violations).deep.equal([]);
