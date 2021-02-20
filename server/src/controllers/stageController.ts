93c2b41ae24b9459e1e617d2fda42e8794215acf
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2021-02-20 22:05:56 +0100

    refactor(#858): StageController from BaseController

--- server/src/controllers/stageController.ts
+++ server/src/controllers/stageController.ts
@@ -1,20 +1,16 @@
 import { Request, Response } from 'express';
 import { Service } from 'typedi';
 
-import {
-  BaseResponse,
-  CollectionApiResponse,
-  ItemApiResponse,
-  Stage,
-  StageCreationAttributes,
-  RequestWithBody
-} from '../models';
+import { BaseResponse, CollectionApiResponse, Stage, StageCreationAttributes, StageAttributes } from '../models';
 import { sendResponse } from './utils';
 import { StageService } from '../services';
+import { BaseController } from './base/BaseController';
 
 @Service()
-export class StageController {
-  constructor(private readonly stageService: StageService) {}
+export class StageController extends BaseController<StageCreationAttributes, StageAttributes, Stage> {
+  constructor(readonly stageService: StageService) {
+    super();
+  }
 
   public async getAll(req: Request, res: Response<CollectionApiResponse<any> | BaseResponse>): Promise<void> {
     req.log.info(`Selecting stages list...`);
@@ -22,16 +18,5 @@ export class StageController {
     const result = await this.stageService.getAll(req.user.OrganizationId);
 
     return sendResponse<CollectionApiResponse<any>, BaseResponse>(result, res);
-  }
-
-  public async create(
-    req: RequestWithBody<StageCreationAttributes>,
-    res: Response<ItemApiResponse<Stage> | BaseResponse>
-  ): Promise<void> {
-    req.log.info(`Creating new stage...`);
-
-    const result = await this.stageService.create(req.body);
-
-    return sendResponse<ItemApiResponse<Stage>, BaseResponse>(result, res);
   }
 }
