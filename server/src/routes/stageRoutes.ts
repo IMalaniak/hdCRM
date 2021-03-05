576a0781e541cc15b5d769734913ee361d3a1c40
Ivan Malaniak <dev@ivanmalaniak.pp.ua>
2021-03-05 12:27:26 +0100

    refactor(#857): refactored routes to use baseRoutes

--- server/src/routes/stageRoutes.ts
+++ server/src/routes/stageRoutes.ts
@@ -1,26 +1,12 @@
-import { Request, Response, Router } from 'express';
 import { Service } from 'typedi';
 
-import { Stage, RequestWithBody, CollectionApiResponse, ItemApiResponse, StageCreationAttributes } from '../models';
+import { BaseRoutes } from './base/baseRoutes';
+import { Stage, StageCreationAttributes, StageAttributes } from '../models';
 import { StageController } from '../controllers';
 
 @Service()
-export class StageRoutes {
-  private router: Router = Router();
-
-  constructor(private readonly stageController: StageController) {}
-
-  public register(): Router {
-    this.router.post(
-      '/',
-      async (req: RequestWithBody<StageCreationAttributes>, res: Response<ItemApiResponse<Stage>>) =>
-        this.stageController.create(req, res)
-    );
-
-    this.router.get('/', (req: Request, res: Response<CollectionApiResponse<Stage>>) =>
-      this.stageController.getAll(req, res)
-    );
-
-    return this.router;
+export class StageRoutes extends BaseRoutes<StageCreationAttributes, StageAttributes, Stage> {
+  constructor(protected readonly routesController: StageController) {
+    super();
   }
 }
